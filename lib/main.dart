f450eaa3e238af712aa9a4c6d553449e52bca8cb
tuna777123 <tuna777123@gmail.com>
2026-07-29 02:05:37 +0300

    feat: track app open at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/app_analytics.dart';
 import 'src/app_observability.dart';
 
 Future<void> main() async {
   MysticAppObservability.configure();
   await MysticAppObservability.run(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await MysticAnalytics.instance.track(MysticAnalyticsEvent.appOpened);
     runApp(const MysticApp());
   });
 }
