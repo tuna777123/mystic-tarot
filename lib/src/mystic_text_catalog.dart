fc9927e05fba4cf2b3bb01e8100904dd9c6bcb68
tuna777123 <tuna777123@gmail.com>
2026-08-02 01:31:08 +0300

    fix: make localization facade constants self-contained

--- lib/src/mystic_text_catalog.dart
+++ lib/src/mystic_text_catalog.dart
@@ -8,8 +8,11 @@ import 'mystic_text_catalog_full.dart' as full;
 class MysticTextCatalog {
   MysticTextCatalog._();
 
-  static const Set<String> launchLanguageCodes =
-      full.MysticTextCatalog.launchLanguageCodes;
+  static const Set<String> launchLanguageCodes = <String>{
+    'ES',
+    'FR',
+    'PT-BR',
+  };
 
   static bool hasTranslation(String languageCode, String english) =>
       full.MysticTextCatalog.hasTranslation(languageCode, english);
