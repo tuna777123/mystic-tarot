42c84b50e4f440e50c595c5e1914a1558c2c1626
tuna777123 <tuna777123@gmail.com>
2026-08-06 01:36:21 +0300

    Initialize the app in the resolved launch language

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/app_locale.dart';
 import 'src/app_lock_gate.dart';
 import 'src/app_observability.dart';
 import 'src/ritual_reminder_service.dart';
@@ -9,6 +10,7 @@ Future<void> main() async {
   MysticAppObservability.configure();
   await MysticAppObservability.run(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await ensureInitialMysticLanguagePreference();
     await RitualReminderService.instance.initialize();
     runApp(const AppLockGate(child: MysticApp()));
   });
