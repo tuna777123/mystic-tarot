829eaab0490f8638b9bfaea9361f7204fa07d958
tuna777123 <tuna777123@gmail.com>
2026-08-06 04:40:03 +0300

    release: define localized store screenshot manifest

---
+++ lib/src/store_screenshot_manifest.dart
@@ -0,0 +1,56 @@
+class StoreScreenshotDevice {
+  const StoreScreenshotDevice({
+    required this.slug,
+    required this.width,
+    required this.height,
+    required this.devicePixelRatio,
+  });
+
+  final String slug;
+  final int width;
+  final int height;
+  final double devicePixelRatio;
+
+  double get logicalWidth => width / devicePixelRatio;
+  double get logicalHeight => height / devicePixelRatio;
+}
+
+enum StoreScreenshotScene {
+  dailyGuidance('01-daily-guidance'),
+  explainableReading('02-explainable-reading'),
+  mysticMirror('03-mystic-mirror'),
+  livingPath('04-living-path'),
+  mysticPlus('05-mystic-plus');
+
+  const StoreScreenshotScene(this.slug);
+
+  final String slug;
+}
+
+const storeScreenshotLocales = <String>['en', 'tr', 'es', 'fr', 'pt-BR'];
+
+const storeScreenshotDevices = <StoreScreenshotDevice>[
+  StoreScreenshotDevice(
+    slug: 'apple-6.7',
+    width: 1290,
+    height: 2796,
+    devicePixelRatio: 3,
+  ),
+  StoreScreenshotDevice(
+    slug: 'google-play-phone',
+    width: 1080,
+    height: 1920,
+    devicePixelRatio: 3,
+  ),
+];
+
+int get expectedStoreScreenshotCount =>
+    storeScreenshotLocales.length *
+    storeScreenshotDevices.length *
+    StoreScreenshotScene.values.length;
+
+String storeScreenshotRelativePath({
+  required StoreScreenshotDevice device,
+  required String locale,
+  required StoreScreenshotScene scene,
+}) => '${device.slug}/$locale/${scene.slug}.png';
