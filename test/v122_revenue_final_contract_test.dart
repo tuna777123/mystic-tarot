6434537625801de650c535be6c73159433d88377
tuna777123 <tuna777123@gmail.com>
2026-08-10 23:31:33 +0300

    Apply Dart formatting to revenue contract

--- test/v122_revenue_final_contract_test.dart
+++ test/v122_revenue_final_contract_test.dart
@@ -42,18 +42,9 @@ void main() {
     expect(adService, contains('ConsentInformation.instance.canRequestAds()'));
     expect(adService, contains('AppOpenAd.load'));
     expect(adService, contains('InterstitialAd.load'));
-    expect(
-      adService,
-      contains('AdExperiencePolicy.minimumAppOpenInterval'),
-    );
-    expect(
-      adService,
-      contains('AdExperiencePolicy.minimumBackgroundDuration'),
-    );
-    expect(
-      adService,
-      contains('AdExperiencePolicy.interstitialEveryReadings'),
-    );
+    expect(adService, contains('AdExperiencePolicy.minimumAppOpenInterval'));
+    expect(adService, contains('AdExperiencePolicy.minimumBackgroundDuration'));
+    expect(adService, contains('AdExperiencePolicy.interstitialEveryReadings'));
     expect(adPolicy, contains('minimumAppOpenInterval = Duration(hours: 2)'));
     expect(
       adPolicy,
