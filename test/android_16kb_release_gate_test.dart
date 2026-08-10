102ec21cb36bea1c54ab888659c496ff2f2091f0
tuna777123 <tuna777123@gmail.com>
2026-08-11 00:32:28 +0300

    Test 16 KB ELF LOAD alignment

--- test/android_16kb_release_gate_test.dart
+++ test/android_16kb_release_gate_test.dart
@@ -5,6 +5,7 @@ import '../tool/src/android_bundle_audit.dart';
 void main() {
   test('locks the Google Play native-library bundle alignment to 16 KB', () {
     expect(requiredGooglePlayPageAlignment, 'PAGE_ALIGNMENT_16K');
+    expect(minimumGooglePlayElfLoadAlignmentBytes, 16 * 1024);
   });
 
   test('accepts bundletool config requesting 16 KB page alignment', () {
@@ -66,6 +67,49 @@ legacy_alignment: PAGE_ALIGNMENT_4K
       () => validateGooglePlayPageAlignment(
         'alignment: PAGE_ALIGNMENT_16K_COMPAT',
       ),
+      throwsA(isA<AuditFailure>()),
+    );
+  });
+
+  test('parses and accepts 16 KB ELF LOAD program-header alignment', () {
+    const output = '''
+Elf file type is DYN (Shared object file)
+Program Headers:
+  Type           Offset   VirtAddr           PhysAddr           FileSiz  MemSiz   Flg Align
+  LOAD           0x000000 0x0000000000000000 0x0000000000000000 0x001000 0x001000 R E 0x4000
+  LOAD           0x004000 0x0000000000004000 0x0000000000004000 0x002000 0x002000 RW  0x10000
+''';
+
+    expect(parseElfLoadAlignments(output), <int>[0x4000, 0x10000]);
+    expect(
+      () => validateGooglePlayElfLoadAlignment(
+        output,
+        label: 'base/lib/arm64-v8a/libapp.so',
+      ),
+      returnsNormally,
+    );
+  });
+
+  test('rejects a 4 KB ELF LOAD program-header segment', () {
+    const output = '''
+Program Headers:
+  Type           Offset   VirtAddr           PhysAddr           FileSiz  MemSiz   Flg Align
+  LOAD           0x000000 0x0000000000000000 0x0000000000000000 0x001000 0x001000 R E 0x1000
+  LOAD           0x004000 0x0000000000004000 0x0000000000004000 0x002000 0x002000 RW  0x4000
+''';
+
+    expect(
+      () => validateGooglePlayElfLoadAlignment(
+        output,
+        label: 'base/lib/x86_64/libapp.so',
+      ),
+      throwsA(isA<AuditFailure>()),
+    );
+  });
+
+  test('rejects ELF output without LOAD segments', () {
+    expect(
+      () => parseElfLoadAlignments('There are no program headers.'),
       throwsA(isA<AuditFailure>()),
     );
   });
